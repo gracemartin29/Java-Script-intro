9c81557a2cb91c6de7be12138f8f260a5c1e1432
gracemartin29 <grace.martin@student.norwichuni.ac.uk>
2024-10-10 22:05:53 +0100

    ahhhh

--- js/script.js
+++ js/script.js
@@ -7,19 +7,20 @@ const image4 = ("assets/Last_of_Us/Tommy_Seattle_Profile.webp");
 const image5 = ("assets/Last_of_Us/Jesse-TLOU.webp");
 const image6 = ("assets/Last_of_Us/TLOU2-Maria-portrait.webp");
 // dbh
-const image7 =("/assets/Detriot_Become_Human/Connor_JP_site.webp")
-const image8 =("/assets/Detriot_Become_Human/Kara_JP_site.webp")
-const image9 =("/assets/Detriot_Become_Human/Detroit-become-human-image-jp-character-deatail-3-img-pc.webp")
+const image7 = ("/assets/Detriot_Become_Human/Connor_JP_site.webp")
+const image8 = ("/assets/Detriot_Become_Human/Kara_JP_site.webp")
+const image9 = ("/assets/Detriot_Become_Human/Detroit-become-human-image-jp-character-deatail-3-img-pc.webp")
 
 // defines HTML constants
 const myButton = document.getElementById("my-button");
+const myButton2 = document.getElementById("my-button2")
 const myParagraph = document.getElementById("my-paragraph");
 const characterImage = document.getElementById("selected-character");
 
 //  let variables
 let charcterCounter = 2;
 
-// button function
+// button function 1
 
 /**
  * update charcter counter and display next charcter
@@ -38,8 +39,8 @@ function clickFunction() {
     charcterCounter += 1;
 
     // finds images depending on charcter counter
-    if(charcterCounter > 6){
-        charcterCounter =1;
+    if (charcterCounter > 6) {
+        charcterCounter = 1;
     }
 
     if (charcterCounter === 1) {
@@ -74,7 +75,37 @@ function clickFunction() {
 
 }
 
-// button
 myButton.onclick = clickFunction;
 
+// button function 2
+function clickFunction2() {
+    // shows clicks in inspect --> console, used to check code is working
+    console.log("the button has been clicked")
 
+    myParagraph.innerHTML = "I am on charcter " + charcterCounter;
+
+    // update charcter counter
+    charcterCounter += 1;
+
+    // finds images depending on charcter counter
+    if (charcterCounter > 3) {
+        charcterCounter = 1;
+    }
+
+    if (charcterCounter === 1) {
+        characterImage.src = image9;
+        return;
+    }
+
+    if (charcterCounter === 2) {
+        characterImage.src = image7;
+        return;
+    }
+
+    if (charcterCounter === 3) {
+        characterImage.src = image8;
+        return;
+    }
+}
+// button
+myButton2.onclick = clickFunction2;
