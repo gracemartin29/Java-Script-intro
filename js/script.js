bfa6031b6fb8d0656e84041787072e4d260e3b2c
gracemartin29 <grace.martin@student.norwichuni.ac.uk>
2024-10-10 08:15:36 -0700

    charcter changes on button and returns

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 // constant variables
+const image1 = ("assests/Joel_in_The_Last_of_Us.png")
+const image2 = ("assests/Ellie_in_The_Last_of_Us_Part_II.png")
+const image3 = ("assests/Abby_in_The_Last_of_Us_Part_II.png")
+
+
+
 const myButton = document.getElementById("my-button");
 const myParagraph = document.getElementById("my-paragraph")
 const characterImage = document.getElementById("character1");
 
 //  let variables
-let clickCounter = 0;
+let charcterCounter = 1;
 
 // button function
 function clickFunction() {
@@ -12,14 +18,34 @@ function clickFunction() {
     // shows clicks in inspect --> console, used to check code is working
     console.log("the button has been clicked")
 
-    // expanded version of line below: clickCounter = clickCounter + 1;
-    clickCounter += 1;
+    myParagraph.innerHTML = "I am on charcter " + charcterCounter;
 
-    myParagraph.innerHTML = "I have been clicked " + clickCounter + " times(s)";
+    // expanded version of line below: clickCounter = clickCounter + 1;
+    charcterCounter += 1;
+
+    if(charcterCounter > 3){
+        charcterCounter =1;
+    }
+
+    if (charcterCounter === 1) {
+        characterImage.src = image1;
+        return;
+    }
+
+    if (charcterCounter === 2) {
+        characterImage.src = image2;
+        return;
+    }
+
+    if (charcterCounter === 3) {
+        characterImage.src = image3;
+        return;
+    }
 
     characterImage.src = "assests/Ellie_in_The_Last_of_Us_Part_II.png";
 }
 
 // button
 myButton.onclick = clickFunction;
+
 
