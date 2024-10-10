5896aebab8af799126f744940ebd6bbdfc5de3fa
gracemartin29 <grace.martin@student.norwichuni.ac.uk>
2024-10-10 07:56:42 -0700

    changes image

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // constant variables
 const myButton = document.getElementById("my-button");
 const myParagraph = document.getElementById("my-paragraph")
+const characterImage = document.getElementById("character1");
 
 //  let variables
 let clickCounter = 0;
@@ -15,7 +16,10 @@ function clickFunction() {
     clickCounter += 1;
 
     myParagraph.innerHTML = "I have been clicked " + clickCounter + " times(s)";
+
+    characterImage.src = "assests/Ellie_in_The_Last_of_Us_Part_II.png";
 }
 
 // button
 myButton.onclick = clickFunction;
+
