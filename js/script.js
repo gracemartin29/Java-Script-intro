9c923b8a8e135ca779a734e7ae0a668eb1bc8c53
gracemartin29 <grace.martin@student.norwichuni.ac.uk>
2024-10-10 07:23:00 -0700

    clicked this many times button

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@
 const myButton = document.getElementById("my-button");
 const myParagraph = document.getElementById("my-paragraph")
 
-// button
-myButton.onclick = clickFunction;
+//  let variables
+let clickCounter = 0;
 
 // button function
 function clickFunction() {
-    myParagraph.innerHTML = "changed paragraph";
+
+    // expanded version of line below: clickCounter = clickCounter + 1;
+    clickCounter += 1;
+
+    myParagraph.innerHTML = "I have been clicked " + clickCounter + " times(s)";
 }
+
+// button
+myButton.onclick = clickFunction;
