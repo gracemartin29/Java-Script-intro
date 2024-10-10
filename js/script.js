fd8eb00e962d8f3dddd17e8ccbb3dcf89fc0b95b
gracemartin29 <grace.martin@student.norwichuni.ac.uk>
2024-10-10 08:21:49 -0700

    comenting

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-// constant variables
+// defines image sources for charcter choice
 const image1 = ("assests/Joel_in_The_Last_of_Us.png")
 const image2 = ("assests/Ellie_in_The_Last_of_Us_Part_II.png")
 const image3 = ("assests/Abby_in_The_Last_of_Us_Part_II.png")
 
-
-
+// defines HTML constants
 const myButton = document.getElementById("my-button");
 const myParagraph = document.getElementById("my-paragraph")
 const characterImage = document.getElementById("character1");
@@ -13,6 +12,13 @@ const characterImage = document.getElementById("character1");
 let charcterCounter = 1;
 
 // button function
+
+/**
+ * update charcter counter and display next charcter
+ * sets counter back to one when three is passed
+ * @returns when charcter is chosen
+ */
+
 function clickFunction() {
 
     // shows clicks in inspect --> console, used to check code is working
@@ -20,9 +26,10 @@ function clickFunction() {
 
     myParagraph.innerHTML = "I am on charcter " + charcterCounter;
 
-    // expanded version of line below: clickCounter = clickCounter + 1;
+    // update charcter counter
     charcterCounter += 1;
 
+    // finds images depending on charcter counter
     if(charcterCounter > 3){
         charcterCounter =1;
     }
@@ -42,7 +49,6 @@ function clickFunction() {
         return;
     }
 
-    characterImage.src = "assests/Ellie_in_The_Last_of_Us_Part_II.png";
 }
 
 // button
